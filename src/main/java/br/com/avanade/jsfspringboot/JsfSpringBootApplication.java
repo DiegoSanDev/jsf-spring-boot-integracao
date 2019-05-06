a2cea2733d5698cdcbc0d87abd058440c3117f6d
Diego Santos <diegosan.dev@gmail.com>
2019-05-06 14:45:39 -0300

    projeto iniciado

---
+++ src/main/java/br/com/avanade/jsfspringboot/JsfSpringBootApplication.java
@@ -0,0 +1,40 @@
+package br.com.avanade.jsfspringboot;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.ServletContextAware;
+
+@SpringBootApplication
+public class JsfSpringBootApplication implements ServletContextAware {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsfSpringBootApplication.class, args);
+	}
+
+	
+	@Bean
+	public ServletRegistrationBean<FacesServlet> servletRegistrationBean() {
+		FacesServlet servlet = new FacesServlet();
+		ServletRegistrationBean<FacesServlet> servletRegistrationBean = new ServletRegistrationBean<>(servlet,"*.xhtml");
+		servletRegistrationBean.setLoadOnStartup(1);
+		return servletRegistrationBean;
+	}
+	
+	@Bean
+	public ServletContextInitializer servletContextInitializer() {
+		return sc -> {};
+	}
+	
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+	}
+
+
+}
